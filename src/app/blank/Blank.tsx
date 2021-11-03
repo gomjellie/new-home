7d4acd259e47867e0d0c553c0a4fe1301d746906
gomjellie <gomjellie@gmail.com>
2021-11-03 20:02:05 +0900

    준철님 웹 사이트 링크 추가

--- src/app/blank/Blank.tsx
+++ src/app/blank/Blank.tsx
@@ -14,7 +14,8 @@ export const Blank = ({ ...props }: BlankProps) => {
       <HelloWorldText>
         <a href="https://github.com/pjc0247">pjc0247</a>님의{" "}
         <a href="https://github.com/pjc0247/new-home/issues/2">허락</a>을 받아
-        <a href="">포트폴리오</a> 프로젝트를 참고해서 만들었습니다.
+        <a href="pjc0247.github.io/new-home">포트폴리오</a> 프로젝트를 참고해서
+        만들었습니다.
         <br />
         pjc0247님께 다시한번 감사드립니다!
       </HelloWorldText>
